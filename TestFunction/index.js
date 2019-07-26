6107d4a1c37b3b4ecb5bc852ec9cc280fa5952f4
Michael Milazzo <1437369+milazzom@users.noreply.github.com>
2019-07-26 00:09:04 -0400

    Adds additional logging.

--- TestFunction/index.js
+++ TestFunction/index.js
@@ -5,29 +5,29 @@ module.exports = async function (context, req) {
     if (req.query.name || (req.body && req.body.name)) {
         const rp = require('request-promise');
 
-        const $ = require('cheerio')
+        const $ = require('cheerio');
 
-        const URL = 'http://www.google.com/search?start=0&num=20&q=%22US%20navy%22'
+        const URL = 'http://www.google.com/search?start=0&num=20&q=%22US%20navy%22';
 
        rp(URL)
         .then(function (html) {
-            context.log("Requested data received.");
+            console.log("Requested data received.");
             $('.jfp3ef', html).each((index, value) => {
                 var link = $(value).children().attr('href')
-                context.log("Link value: " + link);
+                console.log("Link value: " + link);
                 if (typeof (link) === 'string') {
-                    var it = link.split('/url?q=')[1]
-                    urlLinks.push(it)
+                    var it = link.split('/url?q=')[1];
+                    urlLinks.push(it);
                 }
             })
-            context.log("Number of links: " + urlLinks.length);
+            console.log("Number of links: " + urlLinks.length);
 
         }).then(data => {
-            console.log("content length = ", urlLinks.length)
+            console.log("content length = ", urlLinks.length);
 
             context.res = {
                 status: 200, /* Defaults to 200 */
-                body: "Hello Bob" + JSON.stringify(urlLinks)
+                body: "Hello Bob" + JSON.stringify(urlLinks);
             };
             if (urlLinks.length > 0) {
                 // content.forEach((it, index) => {
@@ -38,8 +38,8 @@ module.exports = async function (context, req) {
                     status: 200, /* Defaults to 200 */
                     body: "Hello Bob" + it
                 };
-                context.bindings.outputblob = it
-                context.done()
+                context.bindings.outputblob = it;
+                context.done(null, { outputblob: it });
                 // var it = JSON.stringify(urlLinks)
                 console.log(it)
                 // writer.write(it)
@@ -47,7 +47,7 @@ module.exports = async function (context, req) {
             }
         })
         .catch(function (error) {
-            console.log(error)
+            console.log(error);
         }) 
     
     }
